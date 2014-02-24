2fd67364ae6c9566360daf0017b88c46d5bcbde4
Martin Hansson <minihansson@hotmail.com>
2014-02-24 20:38:53 +0100

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,45 @@
-function sortUnorderedList(ul, sortDescending) {
-  if(typeof ul == "string")
-    ul = document.getElementById(ul);
+// function sortUnorderedList(ul, sortDescending) {
+//   if(typeof ul == "string")
+//     ul = document.getElementById(ul);
 
-  var lis = ul.getElementsByTagName("LI");
-  var vals = [];
+//   var lis = ul.getElementsByTagName("LI");
+//   var vals = [];
 
-  for(var i = 0, l = lis.length; i < l; i++)
-    vals.push(lis[i].innerHTML);
+//   for(var i = 0, l = lis.length; i < l; i++)
+//     vals.push(lis[i].innerHTML);
 
-  vals.sort();
+//   vals.sort();
 
-  if(sortDescending)
-    vals.reverse();
+//   if(sortDescending)
+//     vals.reverse();
 
-  for(var i = 0, l = lis.length; i < l; i++)
-    lis[i].innerHTML = vals[i];
-}
+//   for(var i = 0, l = lis.length; i < l; i++)
+//     lis[i].innerHTML = vals[i];
+// }
 
-window.onload = function() {
-  var desc = false;
-  document.getElementById("sort-button").onclick = function() {
-    sortUnorderedList("sort-list", desc);
-    desc = !desc;
+// window.onload = function() {
+//   var desc = false;
+//   document.getElementById("sort-button").onclick = function() {
+//     sortUnorderedList("sort-list", desc);
+//     desc = !desc;
+//     return false;
+//   }
+//   document.getElementById("sort-button-widget").onclick = function() {
+//     sortUnorderedList("sort-list-widget", desc);
+//     desc = !desc;
+//     return false;
+//   }
+// }   
+
+$('li.expandable').click(function() {
+    $(this).children('ul').toggle();
     return false;
-  }
-  document.getElementById("sort-button-widget").onclick = function() {
-    sortUnorderedList("sort-list-widget", desc);
-    desc = !desc;
-    return false;
-  }
-}     
+});  
+
+$("#child_22").fadeOut(100);
+
+collapseid.onclick - event (e)
+ 
+  
+function hide_(childid)
+ $(# + 'childid).fadeIn(100);
